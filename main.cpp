8188bc5dd1bfb81330ff0de65b24c8ad0067d996
dark-night-men <visible.ocean@gmail.com>
2016-07-16 21:12:56 +0300

    print hello world program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+string s[] = 
+{
+
+  "#include <iostream>"
+, "using namespace std;"
+, "int main(int argc, char **argv)"
+, "{"
+, "    std::cout << \"Hello world\" << std::endl;"
+, "    return 0;"
+, "}"
+
+};
+
 int main(int argc, char **argv)
 {
-    std::cout << "Hello world" << std::endl;
+    cout << "Hello world" << endl;
+
+    for (auto line : s)
+        cout << line << endl;
+
     return 0;
 }
